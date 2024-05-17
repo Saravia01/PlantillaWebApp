c94831ad565ab6b3e7554d42a298dc37fe2a2b6a
marilu1234 <13345@utl.edu.mx>
2024-05-17 08:55:08 -0600

    cracion de PersonaModel
    cree PersonaModel

---
+++ Models/PersonaModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+    public class PersonaModel
+    {
+        public int Id { get; set; }
+        public string Nombre{ get; set; }
+        public string ApellidoPaterno  { get; set; }
+        public int ApellidoMaterno{ get; set; }
+        public float Direccion { get; set; }
+      
+    }
+   
