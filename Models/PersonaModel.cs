1d4502f104d7db7d59e78a2650d3bc52c1210390
OutlawAlex <13350@utl.edu.mx>
2024-05-17 08:37:50 -0600

    Creacion de PersonaModel

---
+++ Models/PersonaModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class PersonaModel
+    {
+        public int Id {get; set; }
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Direccion  { get; set; }
+        public string Estatus { get; set; }
+
+    }
