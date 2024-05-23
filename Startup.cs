40509431b94892f448c7fb6bd837e60efb9f6b52
tania0806 <13347@utl.edu.mx>
2024-05-23 09:56:04 -0600

    carreras(service,model,controller)

--- Startup.cs
+++ Startup.cs
@@ -64,6 +64,7 @@ namespace reportesApi
             services.AddSingleton<FuncionesExtraService>();
             services.AddSingleton<ComprasService>();
             services.AddSingleton<PersonaService>();
+            services.AddSingleton<CarrerasService>();
 
 
             services.AddCors();
