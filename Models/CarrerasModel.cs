dbf3ff248bb3add488fe8aa20292b244dea07079
anthony44B <168848191+anthony44B@users.noreply.github.com>
2024-05-23 08:30:54 -0600

    public class CarrerasModel     {         public int Id { get; set; }         public string Nombre { get; set; }         public string Clave { get; set; }         public string Estatus { get; set; }         public string Usuario { get; set; }       }

---
+++ Models/CarrerasModel.cs
@@ -0,0 +1,8 @@
+ public class CarrerasModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Clave { get; set; }
+        public string Estatus { get; set; }
+        public string Usuario { get; set; }  
+    }
