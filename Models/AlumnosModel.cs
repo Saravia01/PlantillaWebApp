67255a916f39b8c2cc816a96f1130bc8e162a5af
FernandoJose09 <13353@utl.edu.mx>
2024-06-11 17:58:38 -0600

    practica

---
+++ Models/AlumnosModel.cs
@@ -0,0 +1,19 @@
+using System;
+namespace reportesApi.Models
+{
+    public class AlumnosModel
+    {
+        
+
+            public int Id { get; set; }
+            public string ApellidoPaterno { get; set; }
+            public string ApellidoMaterno { get; set; }
+            public string Dirreccion { get; set; }
+            public string Matricula { get; set; }
+            public string Nombre { get; set; }
+
+         
+
+
+    }
+}
