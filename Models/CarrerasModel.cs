90135c436c8aecb55f00da38f6d7d826fdb493d7
tania0806 <13347@utl.edu.mx>
2024-06-12 10:44:32 -0600

    Actualizacion

--- Models/CarrerasModel.cs
+++
@@ -1,8 +0,0 @@
- public class CarrerasModel
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Clave { get; set; }
-        public int Estatus { get; set; }
-        public string Usuario { get; set; }  
-    }
