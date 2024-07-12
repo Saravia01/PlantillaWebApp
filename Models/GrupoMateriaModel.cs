15eb9cdb837d23ed8d231b392b9117f075b9972b
Saravia01 <147068606+Saravia01@users.noreply.github.com>
2024-07-12 08:49:08 -0600

    .

--- Models/GrupoMateriaModel.cs
+++ Models/GrupoMateriaModel.cs
@@ -1,35 +1,14 @@
 using System;
 namespace reportesApi.Models
 {
-    public class GetGrupoMateriaModel
+    public class GrupoMateriaModel
     {
         public int Id { get; set; }
         public int IdGrupo { get; set; }
         public int IdMateria{ get; set; }
-        public string Estatus { get; set; }
+        public int Estatus { get; set; }
         public string Fecha { get; set; }
-        public string Usuario { get; set; }
+        public int Usuario { get; set; }
        
     }
-
-    public class InsertGrupoMateriaModel
-    {
-        public int IdGrupo { get; set; }
-        public int IdMateria{ get; set; }
-        public string Estatus { get; set; }
-        public string Fecha { get; set; }
-        public string Usuario { get; set; }
-    }
-
-    public class UpdateGrupoMateriaModel
-    {
-        public int Id { get; set; }
-        public int IdGrupo { get; set; }
-        public int IdMateria{ get; set; }
-        public string Estatus { get; set; }
-        public string Fecha { get; set; }
-        public string Usuario { get; set; }
-       
-    }
-
 }
